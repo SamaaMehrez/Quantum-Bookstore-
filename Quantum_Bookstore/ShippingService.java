e89afd6d385ad603bcfc34078035410030fef10f
SamaaMehrez <161021631+SamaaMehrez@users.noreply.github.com>
2025-07-08 17:18:38 +0300

    Delete Quantum_Bookstore/ShippingService.java

--- Quantum_Bookstore/ShippingService.java
+++
@@ -1,7 +0,0 @@
-package Quantum_Bookstore;
-
-public class ShippingService {
-    public static void send(String address, Book book) {
-        System.out.println("Shipping " + book.title + " to address: " + address);
-    }
-}
